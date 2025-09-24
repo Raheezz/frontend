0aa33e476c8500240cfb4a05a46c3c2d79211705
rahees <mrsrahees@gmail.com>
2025-09-24 23:35:33 +0530

    auth path settled

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -3,20 +3,20 @@ import api from "./api";
 
 // 🔹 Register a new student
 export const registerUser = (data) =>
-  api.post("accounts/register/", data).then((res) => res.data);
+  api.post("auth/register/", data).then((res) => res.data);
 
 // 🔹 Login -> returns tokens
 export const loginUser = (data) =>
-  api.post("accounts/token/", data).then((res) => res.data);
+  api.post("auth/token/", data).then((res) => res.data);
 
 // 🔹 Refresh access token
 export const refreshToken = (data) =>
-  api.post("accounts/token/refresh/", data).then((res) => res.data);
+  api.post("auth/token/refresh/", data).then((res) => res.data);
 
 // 🔹 Get current user profile
 export const getMe = () =>
-  api.get("accounts/me/").then((res) => res.data);
+  api.get("auth/me/").then((res) => res.data);
 
 // 🔹 Update current user profile
 export const updateMe = (formData) =>
-  api.patch("accounts/me/", formData).then((res) => res.data);
+  api.patch("auth/me/", formData).then((res) => res.data);
